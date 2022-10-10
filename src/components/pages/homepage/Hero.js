c549da9cefb702ef733e5feaee9e86654e8cea5d
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2022-10-10 22:57:16 +0200

    fix hero

--- src/components/pages/homepage/Hero.js
+++ src/components/pages/homepage/Hero.js
@@ -59,7 +59,7 @@ export default function Hero() {
       </div>
       <a
         href="#projects"
-        className="text-lg mt-6 py-2 px-5 font-bold bg-green-200 text-slate-700 hover:bg-green-300 hover:text-slate-900 flex justify-center items-center gap-4"
+        className="text-lg mt-10 py-2 px-5 font-bold bg-green-200 text-slate-700 hover:bg-green-300 hover:text-slate-900 flex justify-center items-center gap-4"
       >
         {t("homepage.hero.more")}
         {icons.arrowdown}
